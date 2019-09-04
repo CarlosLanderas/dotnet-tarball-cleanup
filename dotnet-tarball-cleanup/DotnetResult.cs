1e6e197b5da2ca56caa7436d20ceb6c6e11f1f7d
Carlos Landeras <carlos.landeras@gmail.com>
2019-09-04 21:54:57 +0200

    Do StdOuput and error str builders internal

--- dotnet-tarball-cleanup/DotnetResult.cs
+++ dotnet-tarball-cleanup/DotnetResult.cs
@@ -6,19 +6,19 @@ namespace dotnet_tarball_cleanup
     public class DotnetResult
     {
         public void AppendToStdError(string err) => StdError.AppendLine(err);
-        
+
         public void AppendToStdOutput(string data) => StdOutput.AppendLine(data);
-        
+
         public int ExitCode => StdError.Length == 0 ? 0 : -1;
-        
+
         public Exception Exception => new Exception(StdError.ToString());
-        
+
         public bool HasError => ExitCode != 0;
-        
-        public StringBuilder StdError = new StringBuilder();
-        
-        public StringBuilder StdOutput = new StringBuilder();
-        
+
+        internal StringBuilder StdError = new StringBuilder();
+
+        internal StringBuilder StdOutput = new StringBuilder();
+
         public string Output => StdOutput.ToString();
     }
 }
